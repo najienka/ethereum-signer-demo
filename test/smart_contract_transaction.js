0301e41b2b1e4f4ad92694b0def2510046d4072e
najienka <nemitariajienka@Nemitaris-MacBook-Pro-2.local>
2022-11-03 02:06:17 +0000

    separate unit tests and update second mnemonic to generate fresh wallet

--- test/smart_contract_transaction.js
+++ test/smart_contract_transaction.js
@@ -22,91 +22,15 @@ describe("Sign and Broadcast Raw Transactions", function () {
         return { erc20, owner, otherAccount };
     }
 
-    describe("ETH transfer transactions", function () {
 
-        it("Should create a new wallet (public+private key pair) and send eth from it to a recipient address", async function () {
-            const { erc20, owner, otherAccount } = await loadFixture(deployERC20Mock);
-
-            expect(owner.address).to.not.equal(ethers.constants.addressZero);
-            let ownerBalance = await owner.getBalance();
-
-            // https://docs.ethers.io/v5/api/signer/#Signer
-            // Create a wallet instance from a mnemonic...
-            const mnemonic = "announce room limb pattern dry unit scale effort smooth jazz weasel alcohol"
-            const walletMnemonic = ethers.Wallet.fromMnemonic(mnemonic)
-
-            // ...or from a private key
-            const walletPrivateKey = new ethers.Wallet(walletMnemonic.privateKey)
-
-            expect(walletMnemonic.address === walletPrivateKey.address).to.equal(true);
-            // true
-
-            // The address as a Promise per the Signer API
-            let walletAddress = await walletMnemonic.getAddress();
-            // '0x71CB05EE1b1F506fF321Da3dac38f25c0c9ce6E1'
-
-            // A Wallet address is also available synchronously
-            walletAddress = walletMnemonic.address;
-            // '0x71CB05EE1b1F506fF321Da3dac38f25c0c9ce6E1'
-
-            // The internal cryptographic components
-            const privateKey = walletMnemonic.privateKey
-            // '0x1da6847600b0ee25e9ad9a52abbd786dd2502fa4005dd5af9310b7cc7a3b25db'
-            const publicKey = walletMnemonic.publicKey
-            // '0x04b9e72dfd423bcf95b3801ac93f4392be5ff22143f9980e
-
-            // send ETH to the created wallet address
-            // Ether amount to send
-            // Convert currency unit from ether to wei
-            let amountInEther = ethers.utils.parseEther('2.0');
-            // Create a transaction object
-            let receiverAddress = walletAddress;
-            let tx = {
-                to: receiverAddress,
-                value: amountInEther
-            }
-
-            expect(await ethers.provider.getBalance(receiverAddress)).to.be.equal(0);
-
-            // Send a transaction
-            let txObj = await otherAccount.sendTransaction(tx);
-
-            expect(await ethers.provider.getBalance(receiverAddress)).to.be.equal(amountInEther);
-
-            tx = {
-                to: owner.address,
-                value: ethers.utils.parseEther('0.5'),
-            };
-            // console.log(tx);
-
-            // The connect method returns a new instance of the
-            // Wallet connected to a provider
-            const wallet = walletMnemonic.connect(ethers.provider)
-            // This should return a copy of transactionRequest, follow the same procedure as checkTransaction and fill in any properties required for sending a transaction. The result should have all promises resolved; if needed the resolveProperties utility function can be used for this.
-            // The default implementation calls checkTransaction and resolves to if it is an ENS name, adds gasPrice, nonce, gasLimit and chainId based on the related operations on Signer.
-            // https://docs.ethers.io/v5/api/signer/#Signer-populateTransaction
-            tx = await wallet.populateTransaction(tx);
-            // console.log(tx);
-
-            // Signing a transaction
-            let signedTx = await walletMnemonic.signTransaction(tx);
-
-            let txData = await ethers.provider.sendTransaction(signedTx);
-            // console.log(txData);
-
-            expect(await owner.getBalance()).to.be.equal(ownerBalance.add(ethers.utils.parseEther('0.5')));
-            expect(parseFloat(ethers.utils.formatEther(await wallet.getBalance()))).to.be.lessThanOrEqual(1.5);
-        });
-
-    });
-
-    describe("Smart contract transactions", function () {
+    describe("Smart contract transaction", function () {
         it("Should create a new wallet (public+private key pair) and send erc20 tokens from it using ERC20 token smart contract to a recipient address", async function () {
             const { erc20, owner, otherAccount } = await loadFixture(deployERC20Mock);
 
             // https://docs.ethers.io/v5/api/signer/#Signer
             // Create a wallet instance from a mnemonic...
-            const mnemonic = "announce room limb pattern dry unit scale effort smooth jazz weasel alcohol"
+            // online mnemonic generator tool - https://iancoleman.io/bip39/
+            const mnemonic = "air believe novel unlock ladder bacon cotton timber tobacco hip music guitar powder alarm provide"
             const walletMnemonic = ethers.Wallet.fromMnemonic(mnemonic)
 
             // ...or from a private key
